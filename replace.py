d6824759091f9287c977f5c46ee4323245e69d1b
zuquanzhi <zuquanzhi@qq.com>
2025-02-23 16:08:01 +0800

    修改了replace的问题

--- replace.py
+++ replace.py
@@ -3,84 +3,132 @@ import os
 import math
 import numpy as np
 import random
+from tqdm import tqdm  # 添加tqdm导入
 
 
-# 1. 将四点自适应扩大
-def scale(img, labels, vertical_scale_factor=0.5, horizontal_scale_factor=0.2):
-    """
-    自适应扩大标签的四点坐标
-    :param img: 输入图像
-    :param labels: 图像的标签
-    :param vertical_scale_factor: 纵向拉伸的比例
-    :param horizontal_scale_factor: 横向拉伸的比例
-    :return: 扩大的标签
-    """
+
+# 1、将四点自适应扩大
+def scale(img, labels):
+    # 获取图像大小
     height, width, _ = img.shape
     output_labels = []
 
     for label in labels:
+        # 依次处理每行标签
         temp = label.split(' ')
-        temp = [float(x) for x in temp]
+        for i in range(len(temp)):
+            temp[i] = float(temp[i])
 
         # 将坐标化为绝对坐标
         xyxyxyxy = list(map(lambda x, y: x * y, temp[1:], [width, height, width, height, width, height, width, height]))
 
-        p1, p2, p3, p4 = [list(map(int, xyxyxyxy[i:i+2])) for i in range(0, 8, 2)]
-        
-        # 缩放处理
-        p1_s, p2_s, p3_s, p4_s = p1.copy(), p2.copy(), p3.copy(), p4.copy()
+        p1 = list(map(int, xyxyxyxy[0: 2]))
+        p2 = list(map(int, xyxyxyxy[2: 4]))
+        p3 = list(map(int, xyxyxyxy[4: 6]))
+        p4 = list(map(int, xyxyxyxy[6: 8]))
 
-        # 纵向和横向拉伸处理
-        for p1, p2, scale_factor in [(p1, p2, vertical_scale_factor), (p4, p3, vertical_scale_factor),(p1, p4, horizontal_scale_factor), (p2, p3, horizontal_scale_factor)]:
-            length = np.linalg.norm(np.subtract(p1, p2))
-            angle = math.atan2(p2[1] - p1[1], p2[0] - p1[0]) if p2[0] != p1[0] else math.pi / 2
-            det_x = int(scale_factor * length * math.cos(angle))
-            det_y = int(scale_factor * length * math.sin(angle))
+        # cv.line(img, p1, p2, color=(0, 255, 0), thickness=1)
+        # cv.line(img, p2, p3, color=(0, 255, 0), thickness=1)
+        # cv.line(img, p3, p4, color=(0, 255, 0), thickness=1)
+        # cv.line(img, p4, p1, color=(0, 255, 0), thickness=1)
 
-            p1_s[0] -= det_x
-            p1_s[1] -= det_y
-            p2_s[0] += det_x
-            p2_s[1] += det_y
+        p1_s = p1
+        p2_s = p2
+        p3_s = p3
+        p4_s = p4
+
+        # 纵向拉长p1，p2
+        length1 = pow((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]), 0.5)
+        if p2[0] - p1[0] != 0:
+            theta1 = math.atan((p2[1] - p1[1]) / (p2[0] - p1[0]))
+        else:
+            theta1 = 3.1415926 / 2
+        det1_y = abs(int(length1 * 0.5 * math.sin(theta1)))
+        det1_x = int(theta1 / abs(theta1) * length1 * 0.5 * math.cos(theta1))
+        p1_s[1] -= det1_y
+        p2_s[1] += det1_y
+        p1_s[0] -= det1_x
+        p2_s[0] += det1_x
+
+        # 纵向拉长p4，p3
+        length2 = pow((p4[0] - p3[0]) * (p4[0] - p3[0]) + (p4[1] - p3[1]) * (p4[1] - p3[1]), 0.5)
+        if p3[0] - p4[0] != 0:
+            theta2 = math.atan((p3[1] - p4[1]) / (p3[0] - p4[0]))
+        else:
+            theta2 = 3.1415926 / 2
+        det2_y = abs(int(length2 * 0.5 * math.sin(theta2)))
+        det2_x = int(theta2 / abs(theta2) * length2 * 0.5 * math.cos(theta2))
+        p4_s[1] -= det2_y
+        p3_s[1] += det2_y
+        p4_s[0] -= det2_x
+        p3_s[0] += det2_x
+
+        # cv.line(img, p1_s, p2_s, color=(255, 255, 0), thickness=1)
+        # cv.line(img, p2_s, p3_s, color=(255, 255, 0), thickness=1)
+        # cv.line(img, p3_s, p4_s, color=(255, 255, 0), thickness=1)
+        # cv.line(img, p4_s, p1_s, color=(255, 255, 0), thickness=1)
+
+        # 横向拉长p1，p4
+        length3 = pow((p1[0] - p4[0]) * (p1[0] - p4[0]) + (p1[1] - p4[1]) * (p1[1] - p4[1]), 0.5)
+        theta3 = math.atan((p1[1] - p4[1]) / (p1[0] - p4[0]))
+        det3_x = abs(int(length3 * 0.2 * math.cos(theta3)))
+        det3_y = int(length3 * 0.2 * math.sin(theta3))
+        p1_s[0] -= det3_x
+        p4_s[0] += det3_x
+        p1_s[1] -= det3_y
+        p4_s[1] += det3_y
+
+        # 横向拉长p2，p3
+        length4 = pow((p2[0] - p3[0]) * (p2[0] - p3[0]) + (p2[1] - p3[1]) * (p2[1] - p3[1]), 0.5)
+        theta4 = math.atan((p2[1] - p3[1]) / (p2[0] - p3[0]))
+        det4_x = abs(int(length4 * 0.2 * math.cos(theta4)))
+        det4_y = int(length4 * 0.2 * math.sin(theta4))
+        p2_s[0] -= det4_x
+        p3_s[0] += det4_x
+        p2_s[1] -= det4_y
+        p3_s[1] += det4_y
+
+        # cv.line(img, p1_s, p2_s, color=(0, 0, 255), thickness=1)
+        # cv.line(img, p2_s, p3_s, color=(0, 0, 255), thickness=1)
+        # cv.line(img, p3_s, p4_s, color=(0, 0, 255), thickness=1)
+        # cv.line(img, p4_s, p1_s, color=(0, 0, 255), thickness=1)
 
         output_labels.append(p1_s + p2_s + p3_s + p4_s)
 
     return output_labels
 
-
-# 2. 进行透视变换
+# 2、进行透视变换
+# img1是哨兵图，img2为其他图，传入原来的点，扩大的点，返回变换后的图以及变换后哨兵装甲板的四点坐标
 def tf(img1, label1, label1_s, img2, label2, label2_s):
-    """
-    执行透视变换并替换图像
-    :param img1: 源图像
-    :param label1: 源图像的标签
-    :param label1_s: 源图像的扩展标签
-    :param img2: 目标图像
-    :param label2: 目标图像的标签
-    :param label2_s: 目标图像的扩展标签
-    :return: 变换后的图像和标签
-    """
+    # cv.imshow('img2_ori', img2)
     output_labels = []
-
     for i in range(len(label2_s)):
-        temp1 = np.float32(np.array(label1_s[0]).reshape(4, 2))
-        temp2 = np.float32(np.array(label2_s[i]).reshape(4, 2))
+        temp1 = np.float32([[label1_s[0][0], label1_s[0][1]], [label1_s[0][2], label1_s[0][3]],
+                            [label1_s[0][4], label1_s[0][5]], [label1_s[0][6], label1_s[0][7]]])
+
+        temp2 = np.float32([[label2_s[i][0], label2_s[i][1]], [label2_s[i][2], label2_s[i][3]],
+                            [label2_s[i][4], label2_s[i][5]], [label2_s[i][6], label2_s[i][7]]])
 
         matrix = cv.getPerspectiveTransform(temp1, temp2)
 
         height2, width2, _ = img2.shape
+        height1, width1, _ = img1.shape
         result = cv.warpPerspective(img1, matrix, (width2, height2))
 
         # 制作掩膜
         mask = np.zeros((height2, width2), dtype=np.uint8)
+        # 绘制白色矩形
         points = np.array(np.expand_dims(temp2, axis=0), dtype=np.int32)
         cv.polylines(mask, points, True, (255, 255, 255, 0))
+        # 填充矩形
         cv.fillPoly(mask, points, (255, 255, 255))
+        # 覆盖
         cv.copyTo(result, mask, img2)
 
         # 将标签通过投射变换矩阵进行变换
         folder_label = label1[0].split(' ')
         folder_label = np.array(folder_label[1:], np.float32)
-        folder_label *= ([width2, height2] * 4)
+        folder_label *= ([width1, height1] * 4)
         folder_label = folder_label.reshape(4, 2)
         new_col = [1, 1, 1, 1]
         folder_label = np.column_stack((folder_label, new_col))
@@ -90,82 +138,140 @@ def tf(img1, label1, label1_s, img2, label2, label2_s):
         tf_label[:, :2] /= tf_label[:, 2:3]
         tf_label = np.array(tf_label[:, :2], np.int32)
 
-        newboy = " ".join([f"{round(boy[0] / width2, 6)} {round(boy[1] / height2, 6)}" for boy in tf_label])
+        # 新造一个字符串用来写入
+        newboy = ""
+        for boy in tf_label:
+            newboy = newboy + str(round(boy[0] / width2, 6)) + " " + str(round(boy[1] / height2, 6)) + " "
+        # 去掉最后一个空格
+        newboy = newboy[:-1]
+
         output_labels.append(newboy)
+
+        # cv.line(img2, tf_label[0], tf_label[1], color=(0, 255, 0), thickness=1)
+        # cv.line(img2, tf_label[1], tf_label[2], color=(0, 255, 0), thickness=1)
+        # cv.line(img2, tf_label[2], tf_label[3], color=(0, 255, 0), thickness=1)
+        # cv.line(img2, tf_label[3], tf_label[0], color=(0, 255, 0), thickness=1)
+
+    # cv.imshow('img1', img1)
+    # cv.imshow('img2', img2)
+    # cv.waitKey(0)
 
     return img2, output_labels
 
+# 3、对已有哨兵进行替换
+# 接收三个地址，保存到最后一个地址
+def steal(dir1, dir2, dir3):
+    # 拿出哨兵数据
+    sentry_img_files = []
+    sentry_label_files = []
+    files = os.listdir(dir1)
+    for file in files:
+        name, suf = file.split(".")
+        if suf == 'txt':
+            sentry_label_files.append(file)
+        else:
+            sentry_img_files.append(file)
 
-# 3. 替换哨兵数据
-def steal(src_dir, dst_dir, output_dir, start_index=100, min_category_id=23):
-    print(f"开始处理源图像：{src_dir} 和目标图像：{dst_dir}，输出目录：{output_dir}，起始索引：{start_index}")
 
-    sentry_img_files = [file for file in os.listdir(src_dir) if file.lower().endswith(('.jpg', '.png'))]
-    sentry_label_files = [file for file in os.listdir(src_dir) if file.lower().endswith('.txt')]
-    other_img_files = [file for file in os.listdir(dst_dir) if file.lower().endswith(('.jpg', '.png'))]
-    other_label_files = [file for file in os.listdir(dst_dir) if file.lower().endswith('.txt')]
+        # 拿出其他数据
+    other_img_files = []
+    other_label_files = []
+    new_files = os.listdir(dir2)
+    for new_file in new_files:
+        if os.path.isfile(os.path.join(dir2, new_file)):
+            name, suf = new_file.split(".")
+            if suf == 'txt':
+                other_label_files.append(new_file)
+            else:
+                other_img_files.append(new_file)
 
-    print(f"共找到 {len(sentry_img_files)} 个源图像，{len(other_img_files)} 个目标图像")
-
-    for i, img_file in enumerate(sentry_img_files):
-        print(f"处理第 {i + 1} 个图像...")
-
-        sentry_img = cv.imread(os.path.join(src_dir, img_file))
+    # 遍历哨兵数据
+    for i in tqdm(range(len(sentry_img_files)), desc="处理进度"):
+        # 获取哨兵图片和标签
+        sentry_img = cv.imread(os.path.join(dir1, sentry_img_files[i]))
         sentry_label = []
-        temp_name = os.path.splitext(img_file)[0] + '.txt'
-
-        if not os.path.exists(os.path.join(src_dir, temp_name)):
-            print(f"跳过 {img_file}：缺少标签文件")
+        temp_name, _ = sentry_img_files[i].split('.')
+        if os.path.exists(os.path.join(dir1, temp_name + '.txt')) == False:
             continue
-
-        with open(os.path.join(src_dir, temp_name)) as f:
-            sentry_label = [line.strip() for line in f.readlines()]
-
+        with open(os.path.join(dir1, temp_name + '.txt')) as temp_file:
+            # 只拿一个哨兵装甲板
+            for line in temp_file:
+                sentry_label.append(line)
+                break
+        # 获得自适应扩大的标签
         sentry_label_out = scale(sentry_img, sentry_label)
 
+        # 获得该哨兵装甲板标签（如果为空标签则跳过）
+        if len(sentry_label) == 0:
+            continue
         temp_boys = sentry_label[0].split(' ')
         boy = temp_boys[0]
 
-        # 随机选择目标图像
-        selected_dst_img_file = None
-        for _ in range(10):
-            selected_dst_img_file = random.choice(other_img_files)
-            dst_label_file = os.path.splitext(selected_dst_img_file)[0] + '.txt'
+        # 获取其他图片和标签
+        while True:
+            # 获取下标
+            temp_key = random.randint(0, len(other_img_files) - 1)
+            # 排除掉大装甲板的情况
+            temp_name1, _ = other_img_files[temp_key].split('.')
+            cnt = 0
+            with open(os.path.join(dir2, temp_name1 + '.txt')) as temp_file:
+                for line in temp_file:
+                    temp = line.split(' ')
+                    if temp[0] >= '23':
+                        cnt += 1
+            # 如果没有大装甲板，则可以使用
+            if cnt == 0:
+                break
 
-            if os.path.exists(os.path.join(dst_dir, dst_label_file)):
-                with open(os.path.join(dst_dir, dst_label_file)) as f:
-                    lines = [line.strip().split(' ')[0] for line in f.readlines()]
-                    if all(int(cls) < min_category_id for cls in lines):
-                        break
+        other_img = cv.imread(os.path.join(dir2, other_img_files[temp_key]))
+        other_label = []
 
-        dst_img = cv.imread(os.path.join(dst_dir, selected_dst_img_file))
-        dst_label = []
-        with open(os.path.join(dst_dir, dst_label_file)) as f:
-            dst_label = [line.strip() for line in f.readlines()]
+        with open(os.path.join(dir2, temp_name1 + '.txt')) as temp_file:
+            # 将数据都拿出
+            for line in temp_file:
+                other_label.append(line)
+        # 获得自适应扩大的标签
+        other_label_out = scale(other_img, other_label)
 
-        dst_label_out = scale(dst_img, dst_label)
+        # 进行替换
+        img_out, output_labels = tf(sentry_img, sentry_label, sentry_label_out, other_img, other_label, other_label_out)
 
-        img_out, output_labels = tf(sentry_img, sentry_label, sentry_label_out, dst_img, dst_label, dst_label_out)
-
-        # 保存结果
-        output_index = i + start_index
-        cv.imwrite(os.path.join(output_dir, f'{output_index}.jpg'), img_out)
-
-        with open(os.path.join(output_dir, f'{output_index}.txt'), 'w') as f:
-            for label in output_labels:
-                f.write(f"{boy} {label}\n")
-
-        print(f"图像 {img_file} 处理完成，保存为 {output_index}.jpg")
-
-    print("所有图像处理完成！")
+        # 保存数据
+        cv.imwrite(dir3 + '/' + str(i) + '.jpg', img_out)
+        for j in range(len(output_labels)):
+            output_labels[j] = boy + ' ' + output_labels[j]
+        # 将更改后的标签写入新label文件
+        with open(dir3 + '/' + str(i) + '.txt', 'w') as tem:
+            for line in output_labels:
+                tem.write(line)
 
 
-# 输入配置
-src_dir = "/home/test/Desktop/Tools/test/dst"  # 替换为你的源图像目录
-dst_dir = "/home/test/Desktop/Tools/test/src"  # 替换为你的目标图像目录
-output_dir = "/home/test/Desktop/Tools/test/output"  # 替换为你的输出目录
-start_index = 100  # 起始索引
-min_category_id = 23  # 跳过目标图像中类别ID大于或等于该值的图像
 
-# 执行处理
-steal(src_dir, dst_dir, output_dir, start_index, min_category_id)
+# img1_dir = "/home/horsefly/armor_det/armorDet/mine/resource/HERO-23-OTH-83.jpg"
+# label1_dir = "/home/horsefly/armor_det/armorDet/mine/resource/HERO-23-OTH-83.txt"
+# img1 = cv.imread(img1_dir)
+# labels1 = []
+# with open(label1_dir, 'r') as file:
+#     for line in file:
+#         labels1.append(line)
+# output_labels1 = scale(img1, labels1)
+#
+# img2_dir = "/home/horsefly/armor_det/armorDet/mine/resource/136.png"
+# label2_dir = "/home/horsefly/armor_det/armorDet/mine/resource/136.txt"
+# img2 = cv.imread(img2_dir)
+# labels2 = []
+# with open(label2_dir, 'r') as file:
+#     for line in file:
+#         labels2.append(line)
+# output_labels2 = scale(img2, labels2)
+#
+# tf(img1, labels1, output_labels1, img2, labels2, output_labels2)
+
+# dir1 = "/home/horsefly/下载/hero哨兵/train"
+# dir1 = "/home/horsefly/下载/北邮新哨兵/images/train"
+dir1 = "/home/test/Desktop/Data_Enhance/images/new_sentry"
+dir2 = "/home/test/Desktop/Data_Enhance/images/temp"
+dir3 = "/home/test/Desktop/Data_Enhance/output"
+
+steal(dir1, dir2, dir3)
+
--- visual.py
+++ visual.py
@@ -7,66 +7,91 @@ def visualize_annotations(image_path, label_path):
     读取图像和同名的标注文件，显示标注点并连接。
     :param image_path: 图像文件路径
     :param label_path: 标注文件路径
+    :return: 处理后的图像
     """
     # 读取图像
     img = cv.imread(image_path)
     
     if img is None:
         print(f"无法加载图像: {image_path}")
-        return
+        return None
     
     # 读取标注文件
     if not os.path.exists(label_path):
         print(f"标注文件不存在: {label_path}")
-        return
+        return None
     
     with open(label_path, 'r') as f:
         for line in f:
-            # 解析标注文件中的坐标
             parts = line.strip().split()
             class_id = parts[0]
-            # 提取坐标信息，转换为浮动坐标，然后映射到图像大小
             coords = [float(x) for x in parts[1:]]
+            points = [(int(coords[i] * img.shape[1]), int(coords[i+1] * img.shape[0])) 
+                     for i in range(0, len(coords), 2)]
             
-            # 假设每四个点构成一个多边形，先将坐标转为整数
-            points = [(int(coords[i] * img.shape[1]), int(coords[i+1] * img.shape[0])) for i in range(0, len(coords), 2)]
-            
-            # 绘制每四个点为一个图形
             for i in range(0, len(points), 4):
-                # 取四个点
                 quadrilateral = points[i:i+4]
-                
-                # 绘制多边形
                 for j in range(len(quadrilateral) - 1):
-                    cv.line(img, quadrilateral[j], quadrilateral[j+1], color=(0, 255, 0), thickness=1)
-                
-                # 如果是闭合的标注，最后一个点和第一个点连接
+                    cv.line(img, quadrilateral[j], quadrilateral[j+1], 
+                           color=(0, 255, 0), thickness=1)
                 if len(quadrilateral) == 4:
-                    cv.line(img, quadrilateral[-1], quadrilateral[0], color=(0, 255, 0), thickness=1)
+                    cv.line(img, quadrilateral[-1], quadrilateral[0], 
+                           color=(0, 255, 0), thickness=1)
             
-            # 绘制每个点
             for point in points:
                 cv.circle(img, point, 3, (255, 0, 0), -1)
             
-            # 计算多边形的中心点，以便显示标签
             if len(points) >= 4:
                 center = np.mean(points[:4], axis=0).astype(int)
-                # 设置标签显示位置，稍微偏移
-                offset_x, offset_y = 10, -10  # 偏移量
-                label_position = (center[0] + offset_x, center[1] + offset_y)
-                # 显示class_id
-                cv.putText(img, class_id, label_position, cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv.LINE_AA)
+                label_position = (center[0] + 10, center[1] - 10)
+                cv.putText(img, class_id, label_position, 
+                          cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv.LINE_AA)
+    
+    return img
 
-    # 显示带标注的图像
-    cv.imshow("Image with annotations", img)
-    cv.waitKey(0)
+def visualize_folder(folder_path):
+    """
+    可视化文件夹中的所有图像及其标注。
+    :param folder_path: 包含图像和标注文件的文件夹路径
+    """
+    # 获取所有图像文件
+    image_files = [f for f in os.listdir(folder_path) 
+                  if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    if not image_files:
+        print(f"在 {folder_path} 中未找到图像文件")
+        return
+    
+    current_index = 0
+    
+    while True:
+        image_file = image_files[current_index]
+        image_path = os.path.join(folder_path, image_file)
+        label_path = os.path.join(folder_path, 
+                                 os.path.splitext(image_file)[0] + '.txt')
+        
+        img = visualize_annotations(image_path, label_path)
+        if img is not None:
+            # 显示当前图像信息
+            info = f"{image_file} ({current_index + 1}/{len(image_files)})"
+            cv.putText(img, info, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+            
+            cv.imshow("Image with annotations", img)
+            
+            # 等待键盘输入
+            key = cv.waitKey(0) & 0xFF
+            
+            if key == ord('q'):  # 按 q 退出
+                break
+            elif key == 83 or key == ord('d'):  # 右箭头或 'd'
+                current_index = (current_index + 1) % len(image_files)
+            elif key == 81 or key == ord('a'):  # 左箭头或 'a'
+                current_index = (current_index - 1) % len(image_files)
+    
     cv.destroyAllWindows()
 
 if __name__ == "__main__":
-    # 输入图像和标注文件的路径
-    image_path = './dataset/24_fq_sy_outpost17.jpg'
-    label_path = './dataset/24_fq_sy_outpost17.txt'
+    # 指定包含图像和标注文件的文件夹路径
+    folder_path = './output'
     
-    # 调用可视化函数
-    visualize_annotations(image_path, label_path)
-
+    # 调用文件夹可视化函数
+    visualize_folder(folder_path)
